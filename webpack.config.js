386fed13efb4cbf767c900f15f322c7b943d3430
Sung M. Kim <sungmkim80@gmail.com>
2019-06-09 02:34:23 -0400

    Added html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = ({ mode }) => ({
   mode,
   output: {
     path: `${__dirname}/dist`,
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
 });
